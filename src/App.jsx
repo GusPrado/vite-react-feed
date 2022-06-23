dcde41501e23273509c04a8fcb391f409a73b154
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-06-22 21:18:02 -0300

    feat: Initial sanitize & add global styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+
+import './styles/global.css';
+function App() {
+  return (
+    <>
+      <Header />
+      <h3>Vite!!!</h3>
+    </>
+  );
+}
+
+export default App;
