b6b244fc98cfd405d0d6d36329effe090b32ec2d
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-06-23 21:20:48 -0300

    feat: Finish Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,16 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://github.com/gusprado.png' />
         <strong>Gus Prado</strong>
         <span>Web developer</span>
       </div>
 
       <footer>
-        <a href='#'>Editar seu perfil</a>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
